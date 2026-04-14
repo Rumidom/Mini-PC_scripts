42034191199963aaa86811670541fce529f24f0c
rumidom <jr.robertocv@gmail.com>
2026-04-14 18:29:50 -0300

    fixing screen library

--- lib/st7789.py
+++ lib/st7789.py
@@ -361,7 +361,64 @@ class ST7789():
         self._set_rows(y0, y1)
         self._write(ST7789_RAMWR)
 
+    def pixel(self, x, y, color):
+        self._set_window(x, y, x, y)
+        self._write(None, _encode_pixel(color))
+    
+    def rect(self, x, y, w, h, color):
+        self.hline(x, y, w, color)
+        self.vline(x, y, h, color)
+        self.vline(x + w - 1, y, h, color)
+        self.hline(x, y + h - 1, w, color)
 
+    def fill_rect(self, x, y, width, height, color):
+        self._set_window(x, y, x + width - 1, y + height - 1)
+        chunks, rest = divmod(width * height, _BUFFER_SIZE)
+        pixel = _encode_pixel(color)
+        if chunks:
+            data = pixel * _BUFFER_SIZE
+            for _ in range(chunks):
+                self._write(None, data)
+        if rest:
+            self._write(None, pixel * rest)
+
+    def fill(self, color):
+        self.fill_rect(0, 0, self.width, self.height, color)
+
+    def line(self, x0, y0, x1, y1, color):
+        # Line drawing function.  Will draw a single pixel wide line starting at
+        # x0, y0 and ending at x1, y1.
+        steep = abs(y1 - y0) > abs(x1 - x0)
+        if steep:
+            x0, y0 = y0, x0
+            x1, y1 = y1, x1
+        if x0 > x1:
+            x0, x1 = x1, x0
+            y0, y1 = y1, y0
+        dx = x1 - x0
+        dy = abs(y1 - y0)
+        err = dx // 2
+        if y0 < y1:
+            ystep = 1
+        else:
+            ystep = -1
+        while x0 <= x1:
+            if steep:
+                self.pixel(y0, x0, color)
+            else:
+                self.pixel(x0, y0, color)
+            err -= dy
+            if err < 0:
+                y0 += ystep
+                err += dx
+            x0 += 1
+            
+    def vline(self, x, y, length, color):
+        self.fill_rect(x, y, 1, length, color)
+
+    def hline(self, x, y, length, color):
+        self.fill_rect(x, y, length, 1, color)
+    
     def blit_buffer(self, buffer, x, y, width, height):
         """
         Copy buffer to display at the given location.
@@ -375,3 +432,5 @@ class ST7789():
         """
         self._set_window(x, y, x + width - 1, y + height - 1)
         self._write(None, buffer)
+
+
