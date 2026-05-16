3885e8cc9519e0a7579dc8a3deb4fb04d6dfb579
rumidom <jr.robertocv@gmail.com>
2026-05-16 12:34:25 -0300

    Update ugif library, and adds gif_carousel_example.py

---
+++ gif_carousel_example.py
@@ -0,0 +1,42 @@
+import os
+import time
+import st7789 as st7789
+from machine import Pin, SPI
+from ugif import gif
+
+screen_width = 240
+screen_height = 240
+screen_rotation = 3
+
+spi = SPI(1,
+          baudrate=31250000,
+          polarity=1,
+          phase=1,
+          bits=8,
+          firstbit=SPI.MSB,
+          sck=Pin(4),
+          mosi=Pin(5))
+
+display = st7789.ST7789(
+    spi,
+    screen_width,
+    screen_height,
+    reset=Pin(9, Pin.OUT),
+    dc=Pin(8, Pin.OUT),
+    backlight=Pin(7, Pin.OUT),
+    rotation=screen_rotation)
+
+def drawToScreen_PixelbyPixel(x, y, color):
+    display.pixel(x, y, color)
+
+display.fill(0)
+gif_files = ['01.gif','02.gif','03.gif','04.gif',]
+gif_files.sort()
+
+while True:
+    for filepath in gif_files:
+        gif_obj = gif('gifs/' + filepath)
+        display.fill(0)
+        for i in range(len(gif_obj.Frames)):
+            gif_obj.BlitFrameToScreen(0, drawToScreen_PixelbyPixel)
+            time.sleep(10)
--- lib/ugif.py
+++ lib/ugif.py
@@ -2,12 +2,19 @@ import struct
 import gc
 import os
 import time
-#import micropython
+import micropython
+from array import array
 
-def ByteArrayReverse(Barr):
-    l = list(Barr)
-    l = l[::-1]
-    return bytearray(l)
+@micropython.viper
+def ByteArrayReverse(Barr:ptr8,BarrOut:ptr8,l:int):
+    for x in range(l):
+        BarrOut[x] = Barr[l-1-x]
+
+@micropython.viper
+def Set_IntBit(Var:int,mask:int,maskindex:int,Varindex:int)->int:
+    if mask >> maskindex & 1:
+        Var = Var | 1<<Varindex
+    return Var
 
 def color565(red, green=0, blue=0):
     """
@@ -18,11 +25,14 @@ def color565(red, green=0, blue=0):
     return (red & 0xF8) << 8 | (green & 0xFC) << 3 | blue >> 3
 
 class gif():
-    def __init__(self,path):
+    def __init__(self,path,x=0,y=0,useram=False,verbose = False):
         src = open(path, "rb")
         Header = self.getHeader(src)
-        self.path = path
         dummybuffer = bytearray(os.stat(path)[6])
+        self.path = path
+        self.x = x
+        self.y = y
+        self.useram = useram
         self.ID_tag = Header[0]
         self.Version = Header[1]
         self.Width = Header[2]
@@ -30,123 +40,202 @@ class gif():
         self.Field = Header[4][0]
         self.Background_ci = Header[5][0]
         self.Pixel_AR = Header[6][0]
-        ColorTableLen = 0
+        
         self.ColorTable = []
         self.ColorTable565 = []
+        self.monocrome = False
+        self.AnimTime = 0
+        self.currentFrameIndex = 0
+        ColorTableLen = 0
         if (self.Field >> 7) & 1:
             ColorTableLen = 2**((self.Field & 0b111)+1)
             self.getColorTable(src,ColorTableLen)
-            self.getColorTable565()
-        print(path)
-        print("Colors: ",ColorTableLen)
-        print("Size: ",self.Width,",",self.Height)
+            if ColorTableLen == 2: 
+                self.getColorTable = [False,True]
+                self.monocrome = True
+            else:
+                self.getColorTable565()
+        else:
+            raise Exception("Only gifs with global color table are supported")
+        
+
         self.Frames = []
+        self.decoded = []
+        self.n_frames = 0
         self.loopcount = 0
-        self.FrameTimes = []
         self.getData(src)
-        print("loopcount: ",self.loopcount)
-        print("frameTimes: ",self.FrameTimes)
+        if verbose:
+            print(path)
+            print("Colors: ",ColorTableLen)
+            print("Size: ",self.Width,",",self.Height)
+            print("Loop count: ",self.loopcount)
+            print("Frames: ",self.n_frames)
+        src.close()
+        
+    def setPosition(self,x,y):
+        self.x = x
+        self.y = y
         
     def getColorTable565(self):
         for color in self.ColorTable:
             self.ColorTable565.append(color565(color[0],color[1],color[2]))
-            
-    def lzw_DecodeToScreen(self,data,callback, palBits,useColor565=True):
-        # code from: https://github.com/qalle2/pygif/blob/main/gifdec.py
-        # decode Lempel-Ziv-Welch (LZW) data (bytes)
-        # palBits: palette bit depth in LZW encoding (2-8)
-        # return: indexed image data (bytes)
-        #outSrc    = open(OutputFilePath, "wb")
-        pos       = 0                 # byte position in LZW data
-        bitPos    = 0                 # bit position within LZW data byte (0-7)
-        codeLen   = palBits + 1       # current length of LZW codes, in bits (3-12)
-        code      = 0                 # current LZW code (0-4095)
-        prevCode  = None              # previous code for dictionary entry or None
-        clearCode = 2 ** palBits      # LZW clear code
-        endCode   = 2 ** palBits + 1  # LZW end code
-        entry     = bytearray()       # reconstructed dictionary entry
-        codeCount = 0                 # number of LZW codes read (statistics only)
-        bitCount  = 0                 # number of LZW bits read (statistics only)
+    
+    @micropython.native
+    def blit(self,arr,callback,startPos,frameSize):
+        scr_y = startPos[1]
+        count = 0
+        for i,byte in enumerate(arr):
+            if self.monocrome:
+                for bit_i in range(8):
+                    bit = (byte >> bit_i) & 1
+                    scr_x = count%frameSize[0] + startPos[0]
+                    if scr_x == startPos[0] and count != 0:
+                        scr_y+= 1
+                    callback(scr_x,scr_y,bit)
+                    count += 1
 
-        # LZW dictionary: index = code, value = entry (reference to another code,
-        # final byte)
-        lzwDict = [(None, i) for i in range(2 ** palBits + 2)]
-        imageDataLen = 0
-        scr_x = 0
-        scr_y = 0
-        while True:
-            # get current LZW code (0-4095) from remaining data:
-            # 1) get the 1-3 bytes that contain the code; equivalent to:
-            # codeByteCnt = ceil((bitPos + codeLen) / 8)
-            codeByteCnt = (bitPos + codeLen + 7) // 8
-            if pos + codeByteCnt > len(data):
-                sys.exit("Unexpected end of file.")
-            codeBytes = data[pos:pos+codeByteCnt]
-            # 2) convert the bytes into an integer (first byte = least significant)
-            code = sum(b << (i * 8) for (i, b) in enumerate(codeBytes))
-            # 3) delete previously-read bits from the end and unnecessary bits
-            # from the beginning; equivalent to:
-            # code = (code >> bitPos) % 2 ** codeLen
-            code = (code >> bitPos) & ((1 << codeLen) - 1)
-
-            # advance byte/bit position so the next code can be read correctly
-            bitPos += codeLen
-            pos += bitPos >> 3  # pos += bitPos // 8
-            bitPos &= 0b111     # bitPos %= 8
-
-            # update statistics
-            codeCount += 1
-            bitCount += codeLen
-            if code == clearCode:
-                # LZW clear code:
-                # reset dict. & code length; don't add dict. entry with next code
-                lzwDict = lzwDict[:2**palBits+2]
-                codeLen = palBits + 1
-                prevCode = None
-            elif code == endCode:
-                break
-            elif code > len(lzwDict):
-                sys.exit("Invalid LZW code.")
             else:
-                # dictionary entry
-                if prevCode is not None:
-                    # add new entry (previous code, first byte of current/previous
-                    # entry)
-                    suffixCode = code if code < len(lzwDict) else prevCode
-                    while suffixCode is not None:
-                        (suffixCode, suffixByte) = lzwDict[suffixCode]
-                    lzwDict.append((prevCode, suffixByte))
-                    prevCode = None
-                # reconstruct and store entry
-                entry =  bytearray(b'')
-                referredCode = code
-                while referredCode is not None:
-                    (referredCode, byte) = lzwDict[referredCode]
-                    entry.append(byte)
-                entry = ByteArrayReverse(entry)
-                for i,byte in enumerate(entry):
-                    count = i+imageDataLen
-                    scr_x = count%self.Width
-                    if scr_x == 0 and count != 0:
-                         scr_y+= 1
+                scr_x = i%frameSize[0] + startPos[0]
+                if scr_x == startPos[0] and i != 0:
+                     scr_y+= 1
+                else:
                     if useColor565:
                         callback(scr_x,scr_y,self.ColorTable565[byte])
                     else:
                         callback(scr_x,scr_y,self.ColorTable[byte])
-                    
-                imageDataLen += len(entry)
-                # prepare to add a dictionary entry
-                if len(lzwDict) < 2 ** 12:
-                    prevCode = code
-                if len(lzwDict) == 2 ** codeLen and codeLen < 12:
-                    codeLen += 1
-            gc.collect()
-            #print(f"LZW data: {codeCount} codes, {bitCount} bits, {imageDataLen} pixels")
     
+
+    @micropython.native
+    def get_CodeValue(self,Code,codeTable,byteTable,ColorTableLen):
+        if Code < ColorTableLen:
+            return Code.to_bytes(1)
+        else:
+            nextCode = Code
+            Barr = bytearray()
+            while nextCode > ColorTableLen:
+                index = nextCode-(ColorTableLen+2)
+                nextCode = codeTable[index]
+                Barr.append(byteTable[index])
+            Barr.append(nextCode)
+            Barr_len = len(Barr)
+            BarrOut = bytearray(Barr_len)
+            ByteArrayReverse(Barr,BarrOut,Barr_len)
+            return BarrOut
+    
+    @micropython.native
+    def lzw_DecompressToScreen(self,src,callback,startPos,frameSize,LZW_Min_Code,useColor565=True,useram=False,monocrome=False):
+        ## LZW algorithm
+        ColorTableLen = 2**LZW_Min_Code
+        ClearCode = ColorTableLen
+        ImgEndCode = ColorTableLen + 1
+        CodeLen = LZW_Min_Code+1
+        lastCode = 0
+        K = 0
+        Code = None
+        Codekey = None
+        indexStream = bytearray()
+        datablock = bytearray(256)
+        datablockIndex = 0
+        codeTable = array('i',[])
+        byteTable = bytearray()
+        byte = 0
+        BitIndex = 0
+        newTableIndex = ImgEndCode + 1
+        ByteCount = 0
+        FirstCodeFlag = False
+        newentry = None
+        
+        ## Output to ram
+        outbit_index = 0
+        outbyte = 0b00000000
+        
+        ## Output to Screen
+        imageDataLen = 0
+        scr_x = startPos[0]
+        scr_y = startPos[1]
+        
+        while True:       
+            CodeKey = 0
+            for i in range(CodeLen):   
+                if ByteCount == 0:
+                    ByteCount = src.read(1)[0]
+                    datablock = src.read(ByteCount)
+                    datablockIndex = 0
+                                
+                if BitIndex == 0:
+                    byte = datablock[datablockIndex]
+                    datablockIndex += 1
+                    ByteCount -= 1
+                
+                if (byte >> BitIndex) & 1:
+                    CodeKey = CodeKey | 1<<i
+                BitIndex += 1
+                if BitIndex == 8:
+                    BitIndex = 0
+            #print(CodeKey,' ',FirstCodeFlag,' ',newTableIndex)
+            if CodeKey == ClearCode:
+                codeTable = array('i',[])
+                byteTable = bytearray()
+                newTableIndex = ImgEndCode
+                CodeLen = LZW_Min_Code+1         
+            elif CodeKey == ImgEndCode:
+                break
+            else:
+                if ImgEndCode == newTableIndex:
+                    newEntry = self.get_CodeValue(CodeKey,codeTable,byteTable,ColorTableLen)
+                else:
+                    if (CodeKey < newTableIndex):
+                        codearr = self.get_CodeValue(CodeKey,codeTable,byteTable,ColorTableLen)
+                        newEntry = codearr
+                        K = codearr[0]
+                    else:
+                        lastcodearr = self.get_CodeValue(lastCode,codeTable,byteTable,ColorTableLen)
+                        K = lastcodearr[0]
+                        newEntry = lastcodearr + bytearray([K])                    
+
+                    codeTable.append(lastCode)
+                    byteTable.append(K)
+
+                        
+                    if newTableIndex >= 2**CodeLen-1:
+                        if CodeLen < 12:
+                            CodeLen += 1
+                        
+                newTableIndex += 1
+                lastCode = CodeKey
+                for i,Entrybyte in enumerate(newEntry):
+                    count = i+imageDataLen
+                    scr_x = count%frameSize[0] + startPos[0]
+                    if scr_x == startPos[0] and count != 0:
+                         scr_y+= 1
+                    if monocrome:
+                        callback(scr_x,scr_y,Entrybyte)
+                        if useram:
+                            outbyte =  outbyte | (Entrybyte << outbit_index)
+                            outbit_index += 1
+                            if outbit_index > 7:
+                                outbit_index = 0 
+                                indexStream.append(outbyte)
+                                outbyte = 0b00000000
+                    else:
+                        if useColor565:
+                            callback(scr_x,scr_y,self.ColorTable565[Entrybyte])
+                        else:
+                            callback(scr_x,scr_y,self.ColorTable[Entrybyte])
+                    gc.collect()
+                imageDataLen += len(newEntry)
+                if useram and not monocrome:
+                    indexStream += newentry
+            gc.collect()
+        if (len(indexStream)>0):
+            self.decoded.append(indexStream)
+            
+    @micropython.native
     def getColorTable(self,src,ColorTableLen):
         for i in range(ColorTableLen):
             self.ColorTable.append((src.read(1)[0],src.read(1)[0],src.read(1)[0]))
     
+    @micropython.native
     def getHeader(self,src):
         ID_tag = src.read(3)
         Version = src.read(3)
@@ -158,7 +247,7 @@ class gif():
         Header = ID_tag,Version,Width,Height,Field,Background_ci,Pixel_AR
         #print(Header)
         return Header
-    
+    @micropython.native
     def ReadSubBlocks(self,src):
         subBlocksList = []
         while True:
@@ -168,7 +257,7 @@ class gif():
             subBlockData = src.read(subBlockLenght)
             subBlocksList.append(subBlockData)
         return subBlocksList    
-    
+    @micropython.native
     def ReadFrameData(self,src):
         gc.collect()
         frameData = bytearray()
@@ -179,72 +268,102 @@ class gif():
             subBlockData = src.read(subBlockLenght)
             frameData.extend(subBlockData)
         return frameData
-    
+    @micropython.native
     def ReadBlock(self,src):
         Block_Dict = {'SubBlocks':[]}
         BlockLenght = src.read(1)[0]
         Block_Dict['BlockData'] = src.read(BlockLenght)
         Block_Dict['SubBlocks'] = self.ReadSubBlocks(src)
         return Block_Dict
-        
+    @micropython.native
     def ReadGraphicsControlBlock(self,src):
         #print('GraphicsControlBlock')
         blockDict = self.ReadBlock(src)
-        delay = blockDict['BlockData'][1]*10
-        delay = delay+ blockDict['BlockData'][2]
-        self.FrameTimes.append(delay)
-        
+        packField = blockDict['BlockData'][0]
+        dispMethod = (packField&0b11100) >> 2
+        # 0 - disposal method not specified
+        # 1 - leave the image in place and draw the next image on top of it
+        # 2 - the canvas should be restored to the background color
+        # 3 - restore the canvas to its previous state before the current image was drawn
+        TranspFlag = packField&1
+        delay = int.from_bytes(blockDict['BlockData'][1:2],'big')*10
+        AnimData = {'anim':[dispMethod,TranspFlag,delay]}
+        self.Frames.append(AnimData)
+        #disposalMethod = 2**((self.Field & 0b111)+1)
+    @micropython.native
     def ReadApplicationBlock(self,src):
         #print('ApplicationBlock')
         blockdict = self.ReadBlock(src)
         if blockdict['BlockData'] == b'NETSCAPE2.0':
             self.loopcount = int.from_bytes(blockdict['SubBlocks'][0][1:], 'little')
-    
+    @micropython.native
     def ReadPlainTextBlock(self,src):
         #print('PlainTextBlock')
         self.ReadBlock(src)
-        
+    @micropython.native        
     def ReadCommentBlock(self,src):
         #print('CommentBlock')
         self.ReadBlock(src)
-    
+    @micropython.native
     def ReadFrame(self,src):
         #print('Frame')
         #print('FreeMem:',gc.mem_free())
         frameMetaData = src.read(9)
         #print('frameMetaData: ',frameMetaData)
+        imageLeft = int.from_bytes(frameMetaData[0:1], 'little') 
+        imageTop = int.from_bytes(frameMetaData[2:3], 'little')
         frameWidth = int.from_bytes(frameMetaData[4:6], 'little') 
-        frameHeight = int.from_bytes(frameMetaData[6:8], 'little') 
+        frameHeight = int.from_bytes(frameMetaData[6:8], 'little')
         localTableFlag = frameMetaData[8] >> 7
         interlaceFlag = frameMetaData[8] >> 6
         BytesToData = src.tell()
         frameLZW_min = src.read(1)[0]
-        FrameDict = {'Meta':{'locc':localTableFlag,'intlc':interlaceFlag},'BytesToData':BytesToData}
-        if frameWidth != self.Width:
-            FrameDict['Meta']['w'] = frameWidth
-        if frameHeight != self.Height:   
-            FrameDict['Meta']['h'] = frameHeight
-        print('FrameDict: ',FrameDict)
+        # imgdata : [imlft,imtop,Width,Height,locc,intlc]
+        FrameDict = {'img':[imageLeft,imageTop,frameWidth,frameHeight,localTableFlag,interlaceFlag],'BytesToData':BytesToData}
+        
+        #change to skip stead of reading
         while True:
             subBlockLenght = src.read(1)[0]
             if subBlockLenght == 0:
                 break
             subBlockData = src.read(subBlockLenght)
             #print(len(subBlockData))
-        self.Frames.append(FrameDict)
+        self.Frames[-1].update(FrameDict)
+        self.n_frames += 1
+        #print('FrameDict: ',self.Frames[-1])
         #print('FreeMem:',gc.mem_free())
-    
-    def BlitToScreen(self,FrameIndex,callback):
-        startTime = time.time()
-        src = open(self.path, "rb")
-        src.seek(self.Frames[FrameIndex]['BytesToData'])
-        frameLZW_min = src.read(1)[0]
-        frameData = self.ReadFrameData(src)
-        src.close()
-        #print('frameData Ready')
-        self.lzw_DecodeToScreen(frameData,callback,frameLZW_min)
-        print("imageBlited, Time: ",time.time()-startTime)
         
+    @micropython.native
+    def BlitFrameToScreen(self,FrameIndex,callback):
+        startTime = time.time()
+        frame_x = self.Frames[FrameIndex]['img'][0]
+        frame_y = self.Frames[FrameIndex]['img'][1]
+        frameSize = (self.Frames[FrameIndex]['img'][2],self.Frames[FrameIndex]['img'][3])
+        startPos = (frame_x+self.x,frame_y+self.y)
+        if len(self.decoded) > FrameIndex:
+            FrameData = self.decoded[FrameIndex]
+            self.blit(FrameData,callback,startPos,frameSize)
+        else:
+            src = open(self.path, "rb")
+            src.seek(self.Frames[FrameIndex]['BytesToData'])
+            frameLZW_min = src.read(1)[0]
+            #frameCompData = self.ReadFrameData(src)
+            #self.lzw_DecodeToScreen(frameCompData,callback,startPos,frameSize,frameLZW_min,monocrome=self.monocrome,useram=self.monocrome)
+            self.lzw_DecompressToScreen(src,callback,startPos,frameSize,frameLZW_min,monocrome=self.monocrome,useram=self.useram)
+            src.close()
+        #print('frameData Ready')
+        #print('start',startPos)
+        #print("imageBlited, Time: ",time.time()-startTime)
+    @micropython.native
+    def BlitAnimationToScreen(self,callback):
+        self.BlitFrameToScreen(self.currentFrameIndex,callback)        
+        if (time.ticks_ms()-self.AnimTime)//self.Frames[self.currentFrameIndex]['anim'][2] > 0:
+            self.AnimTime = time.ticks_ms()
+            self.currentFrameIndex += 1
+            if self.currentFrameIndex > self.n_frames-1:
+                self.currentFrameIndex = 0
+                
+    @micropython.native
     def getData(self,src):
         Supported_Extensions = {
         b'\xf9': self.ReadGraphicsControlBlock,        
